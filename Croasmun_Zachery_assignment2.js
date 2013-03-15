1849d5089feb39df4b4e849b244b1175fe85c386
Zachery croasmun <th3p4ler1der@gmail.com>
2013-03-14 17:28:17 -0700

    second function added

--- Croasmun_Zachery_assignment2.js
+++ Croasmun_Zachery_assignment2.js
@@ -4,12 +4,13 @@
  */
 
 
-alert("this is our story")
+//alert("this is our story")
 
 
 
 var sharkName = "Iron Tooth",
-	fishNames = [" Gill", " Finn", " Fudge"],
+	ironTummy = 51
+	fishNames = ["Gill", "Finn", "Fudge"],
 	fishWeight= [10, 25 , 52],
 	catchTime = [30, 20, 8];
 	
@@ -22,7 +23,7 @@ var fishSeen = function(fishNumber){
 		weight = fishWeight[fishNumber],
 		iCatch = catchTime[fishNumber];
 		
-		console.log(sharkName + " decides to chase " + iSee)
+	console.log(sharkName + " decides to chase " + iSee)
 		
 	if(weight < 51){
 		if(iCatch < 31)
@@ -32,7 +33,29 @@ var fishSeen = function(fishNumber){
 	
 };
 
+fishSeen(1);
 
+var chase = function(fish){
+	var thatFish = fishNames[fish],
+		time = catchTime[fish];
+	
+	console.log("I am going to eat you " + thatFish + "!")
+	console.log("It will take me " + time + " minuets to catch you. I would say your prayers.")
+	while(time > 0){
+		
+		console.log(time + " Minuets till I catch you" + thatFish + "!");
+		time -= 2;
+	};
+	return console.log("Hahahahaha " + thatFish + " was Yummy!");
+};
+
+chase(1);
+
+var newFish = function(){
+	
+	
+	
+};
 
 
 
